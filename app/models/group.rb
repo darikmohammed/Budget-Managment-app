e024efc44cc706a252d7b99a855eab7ac710a191
darikmohammed <r.m.darik@gmail.com>
2022-10-13 15:43:06 +0300

    Add validation of models and rspec

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,8 @@ class Group < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :entities
 
+  validates :name, presence: true
+  validates :icon, presence: true
   def amount
     entities.includes(:groups).sum(:amount)
   end
